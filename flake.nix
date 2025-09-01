c0373ec1885218fd8b154c932db6a73ede827c64
Karsten Finderup Pedersen <kpede22@student.sdu.dk>
2025-09-01 17:36:31 +0200

    Setup Agda

---
+++ flake.nix
@@ -0,0 +1,22 @@
+{
+  description = "Agda Environment";
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+  outputs = { nixpkgs, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+      in
+      {
+        devShell = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            just
+            agda
+            agdaPackages.standard-library
+          ];
+        };
+      }
+  );
+}
